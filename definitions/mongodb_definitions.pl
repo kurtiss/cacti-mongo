006ce52d61202c1666cf6e3e8a73907f68a84f8a
Narayan Newton <nnewton@tag1consulting.com>
2010-03-30 16:18:03 -0700

    MongoDB commands graph

--- definitions/mongodb_definitions.pl
+++ definitions/mongodb_definitions.pl
@@ -52,7 +52,7 @@
          ],
       },
       {  name       => 'MongoDB Memory',
-         base_value => '0',
+         base_value => '1000',
          hash       => 'hash_00_VER_f01951c2799f75c4c1d1bed47eb96e40',
          dt         => {
             hash       => 'hash_01_VER_7e137605a58148cf8a555e6baf20eb65',
@@ -102,6 +102,106 @@
                   'hash_10_VER_d79210d1ffb8a132f70b22c75c3c3c52',
                   'hash_10_VER_55396aa89c98397a4cc3f31b52f87990',
                   'hash_10_VER_c1ba73653931511c45060f267fc6cb30'
+               ],
+            },
+         ],
+      },
+      {  name       => 'MongoDB Commands',
+         base_value => '1000',
+         hash       => 'hash_00_VER_80a7a84bde78767289d31d1ed655afd5',
+         dt         => {
+            hash       => 'hash_01_VER_f5edc729abaf7108fdb225dbef0269d2',
+            input      => 'Get MongoDB Stats',
+            MONGODB_op_inserts => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_99a1475f96923a6424566af7187d8755'
+            },
+            MONGODB_op_queries => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_cbfaa4bf39e56a544d33f1b3dae25f59'
+            },
+            MONGODB_op_updates => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_270098586e9ddb730b228db4db35dc83'
+            },
+            MONGODB_op_deletes => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_a39184cbd72f1710576d2f96cd7c9c01'
+            },
+            MONGODB_op_getmores => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_b7b44cedfe365a8d1591e7b1cf7974cf'
+            },
+            MONGODB_op_commands => {
+               data_source_type_id => '3',
+               hash => 'hash_08_VER_855722b9df77c4a52cdc6dcb5535fdf9'
+            },
+         },
+         items => [
+            {  item   => 'MONGODB_op_inserts',
+               color  => 'FF7200',
+               task   => 'hash_09_VER_51843de5556c83fd6017210bcbe1c8dc',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_25f0c6652a13d2aa625e000b6df313d3',
+                  'hash_10_VER_e6e557c4308cef7f185895bcbb4e7d1c',
+                  'hash_10_VER_a3b2ad85b70ebbc7d61bb43b81409892',
+                  'hash_10_VER_acfc0b6a993edf8d33c9b092f317c9e6'
+               ],
+            },
+            {  item   => 'MONGODB_op_queries',
+               color  => '503001',
+               task   => 'hash_09_VER_bb359d50779d9fa5a00d76b78b6a4b0c',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_afd2d45192c139095bacb9feee1ffab5',
+                  'hash_10_VER_ec73cdf0ae78c03e734d023c14a24a0a',
+                  'hash_10_VER_f564cd40dee74afda778005b8b511e63',
+                  'hash_10_VER_06f0d7c21e83da219532f68b0176c3f8'
+               ],
+            },
+            {  item   => 'MONGODB_op_updates',
+               color  => 'EDAC00',
+               task   => 'hash_09_VER_23bd922edc3460e20607d1fc4d49b3a3',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_0de086b64280315bf8b46137882e7913',
+                  'hash_10_VER_52347bc4d82487b5c1c964f9cfcf365a',
+                  'hash_10_VER_1c40a2119fb0d1744db533e1fe859b4b',
+                  'hash_10_VER_76078fc1ad0b4cdc0b3de91e8b0914f8'
+               ],
+            },
+            {  item   => 'MONGODB_op_deletes',
+               color  => '506101',
+               task   => 'hash_09_VER_a1bdad535e71b4d0586a15bcd95b0db4',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_3e49b88d0953866e87ee17645b394008',
+                  'hash_10_VER_2ef0b64c0cedd6cb628a5163f171c25c',
+                  'hash_10_VER_199f308f7588dea4b0007e45b1835556',
+                  'hash_10_VER_508bff27083eada2973d6cb2176e78e9'
+               ],
+            },
+            {  item   => 'MONGODB_op_getmores',
+               color  => '0CCCCC',
+               task   => 'hash_09_VER_bd1348bd4e29a59053855d9a5991ad34',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_827895cdb5c458e76b3531ca22b65933',
+                  'hash_10_VER_94c1370388158e64b1f14bb0cf972cca',
+                  'hash_10_VER_370af505286ec26fe7940b461369e73b',
+                  'hash_10_VER_f7eee0432160a69e98746e03793533d5'
+               ],
+            },
+            {  item   => 'MONGODB_op_commands',
+               color  => '53CA05',
+               task   => 'hash_09_VER_38b38ffc417def78328030363aaec579',
+               type   => 'LINE1',
+               hashes => [
+                  'hash_10_VER_8670a7133b9b8e062018918554234603',
+                  'hash_10_VER_e0dd370c9a09110551dcad6958da48a0',
+                  'hash_10_VER_7b664bf01401fb6c7f59bd582ab5161b',
+                  'hash_10_VER_19ede2025d22ed99b6b4b9795e9c6f7a'
                ],
             },
          ],
